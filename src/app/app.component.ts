4517351e4aa8b6b8899fb9b4996100f207d2197c
Rameshwar-coforge <rameshwar.gholap@coforgetech.com>
2020-11-29 23:21:52 +0530

    Dynamic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  title = '!!my-first-angular-app!';
 }
